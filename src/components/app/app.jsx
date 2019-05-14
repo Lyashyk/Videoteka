acf09ca5cf2e91d7bc5246f463a7b3d850dee356
Aleksndr Illyashenko <alex1002x@gmail.com>
2019-05-14 14:44:47 +0300

    feat: app

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './app.css';
+
+import VideotekaAppContainer from '../../containers/videoteka-app-container';
+
+function App() {
+  return (
+    <div className="App">
+      <VideotekaAppContainer />
+    </div>
+  );
+}
+
+export default App;
