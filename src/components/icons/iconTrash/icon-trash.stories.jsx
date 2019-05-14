ea777b82607a508f421e657a857eafb9a0b5e0bc
Aleksndr Illyashenko <alex1002x@gmail.com>
2019-05-14 14:58:03 +0300

    feat: icon-storybook

--- src/components/icons/iconTrash/icon-trash.stories.jsx
+++ src/components/icons/iconTrash/icon-trash.stories.jsx
@@ -4,7 +4,7 @@ import IconTrash from './icon-trash';
 
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Components|IconTrash', module).add('Default', () => {
+storiesOf('UI|Icons/IconTrash', module).add('Default', () => {
   return (
     <div style={{ width: '64px', height: '64px' }}>
       <IconTrash />
