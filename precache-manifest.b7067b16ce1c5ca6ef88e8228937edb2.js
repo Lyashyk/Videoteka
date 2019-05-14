4a9f1ba235de9293cbcc694c31c90729873eaf35
Aleksndr Illyashenko <alex1002x@gmail.com>
2019-05-14 15:23:13 +0300

    Updates

---
+++ precache-manifest.b7067b16ce1c5ca6ef88e8228937edb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e77d991aada754094d8d8d06082fa5",
+    "url": "/test-task-videoteka/index.html"
+  },
+  {
+    "revision": "78651802b722db53fe87",
+    "url": "/test-task-videoteka/static/css/main.c6fc2450.chunk.css"
+  },
+  {
+    "revision": "c9682dd7eb087b2a8ced",
+    "url": "/test-task-videoteka/static/js/2.615839ce.chunk.js"
+  },
+  {
+    "revision": "78651802b722db53fe87",
+    "url": "/test-task-videoteka/static/js/main.29addfbf.chunk.js"
+  },
+  {
+    "revision": "b0e62b32ea3e5dd3886e",
+    "url": "/test-task-videoteka/static/js/runtime~main.d505ca84.js"
+  }
+]);
